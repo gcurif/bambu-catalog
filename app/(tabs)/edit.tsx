35268a98392efdee4cfc839c62fcf5c4dcdb04b0
gustavo curifuta <gustavo.curifuta@usach.cl>
2025-08-15 04:16:29 -0400

    feat: modificaciones pantalla de edición

--- app/(tabs)/edit.tsx
+++ app/(tabs)/edit.tsx
@@ -3,10 +3,11 @@ import { View } from 'react-native';
 import { EditItem } from '@/components/EditItem';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 import { getSchema } from '@/data/data';
+import { FilterSchemaItem } from '@/model/schema';
 import { useEffect, useState } from 'react';
 
 export default function HomeScreen() {
-  const [schema, setSchema] = useState<{ id: string }[]>();
+  const [schema, setSchema] = useState<FilterSchemaItem[]>();
 
   useEffect(() => {
     const fetchSchema = async () => {
--- components/EditItem.tsx
+++ components/EditItem.tsx
@@ -2,12 +2,9 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
 import { FilterSchemaItem } from "@/model/schema";
-import React, { useState } from "react";
+import React from "react";
 
 import { ScrollView, StyleSheet, View } from "react-native";
-import FieldSelect from "./common/FieldSelect";
-import ModalAddEdit from "./common/ModalAddEdit";
-import ModalDelete from "./common/ModalDelete";
 
 export interface SearchEngineProps {
   schema: FilterSchemaItem[];
@@ -18,89 +15,23 @@ export const EditItem: React.FC<SearchEngineProps> = ({
   schema = [],
   onSearch,
 }) => {
-  const [filters, setFilters] = useState<Record<string, string>>({});
-  const [showModalAddEdit, setShowModalAddEdit] = useState(false);
-  const [showModalDelete, setShowModalDelete] = useState(false);
-  const [valueToEdit, setValueToEdit] = useState<string>("");
-
-  const handleFilterChange = (key: string, value: string) => {
-    setFilters((prev) => ({ ...prev, [key]: value }));
-  };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Heading style={styles.heading} size="xl">
-        Opciones
+        Modificar Opciones Modificables
       </Heading>
       {schema.map((item, index) => (
         <View key={index} style={styles.filterContainer}>
           {item.type === "option" && (
             <View style={styles.optionContainer}>
-              <FieldSelect
-                placeholder={item.name}
-                value={filters[item.name] || ""}
-                handleChange={(value) => handleFilterChange(item.name, value)}
-                options={(item.options ?? []).map((opt) => ({
-                  label: opt.name,
-                  value: opt.name,
-                  fav: opt.fav || false, // Assuming fav is optional
-                }))}
-              />
-              <View style={styles.buttonGroup}>
-                <Button
-                  size="lg"
-                  style={styles.button}
-                  onPress={() => setShowModalAddEdit(true)}
-                  variant="outline"
-                >
-                  <Text style={styles.buttonText}>Agregar</Text>
-                </Button>
-                <Button
-                  size="lg"
-                  style={styles.button}
-                  onPress={() => {
-                    setShowModalAddEdit(true);
-                    setValueToEdit(filters[item.name]);
-                  }}
-                  variant="outline"
-                >
-                  <Text style={styles.buttonText}>Editar</Text>
-                </Button>
-                <Button
-                  size="lg"
-                  style={styles.button}
-                  onPress={() => setShowModalDelete(true)}
-                  variant="outline"
-                >
-                  <Text style={styles.buttonText}>Eliminar</Text>
-                </Button>
-              </View>
+              <Button style={styles.button} size="xl" onPress={() => null}>
+                <Text style={styles.buttonText}>{item.name}</Text>
+              </Button>
             </View>
           )}
         </View>
       ))}
-      <ModalAddEdit
-        show={showModalAddEdit}
-        onCloseClick={() => setShowModalAddEdit(false)}
-        onConfirm={(value) => {
-          setFilters((prev) => ({ ...prev, [value]: value }));
-          setShowModalAddEdit(false);
-        }}
-        value=""
-        onChangeValue={() => {}}
-      />
-      <ModalDelete
-        show={showModalDelete}
-        onCloseClick={() => setShowModalDelete(false)}
-        onConfirm={() => {
-          setFilters((prev) => {
-            const newFilters = { ...prev };
-            delete newFilters[valueToEdit];
-            return newFilters;
-          });
-          setShowModalDelete(false);
-        }}
-      />
     </ScrollView>
   );
 };
@@ -122,22 +53,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 16,
   },
-  buttonGroup: {
-    flexDirection: "row",
-    marginTop: 8,
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
   button: {
     flex: 1,
     marginRight: 4,
     backgroundColor: "rgba(255, 255, 255, 1)",
-    height: 42,
+    width: "80%",
+    minHeight: 48,
   },
   buttonText: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: "400",
-    color: "#333",
+    color: "#fff",
     textAlign: "center",
   },
 });
